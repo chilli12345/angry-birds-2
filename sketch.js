e231f3c737bb17859fcf4ee508b41d1306539775
chilli12345 <73940796+chilli12345@users.noreply.github.com>
2020-12-03 19:41:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,11 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImage;
-var sling;
+var backgroundImg,platform;
+var bird, slingShot;
 
-function preload(){
-    backgroundImage=loadImage("sprites/bg.png");
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
@@ -17,15 +17,14 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
-    platform = new Ground(150,305,300,170);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -37,19 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
-    
-    sling = new SlingShot(bird.body,{x:200,y:100});
-    
-    
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    background(backgroundImage);
+    background(backgroundImg);
     Engine.update(engine);
-    /*console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);*/
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -66,17 +62,16 @@ function draw(){
     log5.display();
 
     bird.display();
-
     platform.display();
-
-    sling.display();
-
+    //log6.display();
+    slingshot.display();    
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
+
 function mouseReleased(){
-    sling.fly();
+    slingshot.fly();
 }
